57ffd9262a39c08aa2ec831f08cd95191259025f
Ovresko <ovresko@gmail.com>
2018-09-10 15:30:47 +0100

    Doc submit event

---
+++ Framework/Events/DocumentSubmittedEvent.cs
@@ -0,0 +1,22 @@
+﻿using Ovresko.Generix.Datasource.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ovresko.Generix.Core.Framework.Events
+{
+    public class DocumentSubmittedEvent
+    {
+        public DocumentSubmittedEvent(IDocument eventDocument, string eventName)
+        {
+            EventDocument = eventDocument;
+            EventName = eventName;
+        }
+
+        public IDocument EventDocument { get; set; }
+        public string EventName { get; set; }
+
+    }
+}
